42ec10faabd3b19ae5d17c8019012a2b6e1c7674
nair-aditya-04 <nair.aditya.lti@gmail.com>
2025-09-25 21:17:42 +0530

    TeacherController.java

--- backend/src/main/java/com/wecp/progressive/controller/TeacherController.java
+++ backend/src/main/java/com/wecp/progressive/controller/TeacherController.java
@@ -1,12 +1,22 @@
 package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Teacher;
+import com.wecp.progressive.service.impl.TeacherServiceImplArraylist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
-public class TeacherController {
 
+public class TeacherController  {
+ 
     public ResponseEntity<List<Teacher>> getAllTeachers() {
         return null;
     }
@@ -18,9 +28,12 @@ public class TeacherController {
     public ResponseEntity<Integer> addTeacher(Teacher teacher) {
         return null;
     }
+  
 
-    public ResponseEntity<Void> updateTeacher(int teacherId, Teacher teacher) {
-        return null;
+   @PutMapping("/{teacherId}")
+    public ResponseEntity<Void> updateTeacher(int teacherId, Teacher teacher) throws Exception{
+        return new ResponseEntity<>(HttpStatus.OK);
+        
     }
 
     public ResponseEntity<Void> deleteTeacher(int teacherId) {
