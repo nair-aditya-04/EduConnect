a6f850b21cdf29950a9ee888a502eda082248c74
username <wecp-dummy@gmail.com>
2025-09-26 15:15:06 +0000

    Day 8 Task

--- backend/src/main/java/com/wecp/progressive/controller/CourseController.java
+++ backend/src/main/java/com/wecp/progressive/controller/CourseController.java
@@ -1,13 +1,36 @@
 package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Course;
+import com.wecp.progressive.service.CourseService;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/course")
 public class CourseController {
+
+    @Autowired
+    CourseService courseService;
+    @GetMapping
     public ResponseEntity<List<Course>> getAllCourses() {
+        try
+        {
+            return new ResponseEntity<>(courseService.getAllCourses(),HttpStatus.OK);
+        }
+        catch(Exception e)
+        {
+            e.getMessage();
+        }
         return null;
+        
     }
 
     public ResponseEntity<Course> getCourseById(int courseId) {
--- backend/src/main/java/com/wecp/progressive/repository/TeacherRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/TeacherRepository.java
@@ -1,5 +1,11 @@
 package com.wecp.progressive.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TeacherRepository {
+import com.wecp.progressive.entity.Teacher;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+    public Teacher findByTeacherId(int teacherId);
 }
