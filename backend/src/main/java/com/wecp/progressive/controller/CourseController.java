2d1602d470f92a7b6f80d9bf4cab39a0ae721e00
username <wecp-dummy@gmail.com>
2025-09-29 13:25:48 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/controller/CourseController.java
+++ backend/src/main/java/com/wecp/progressive/controller/CourseController.java
@@ -1,21 +1,87 @@
 package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Course;
+import com.wecp.progressive.repository.CourseRepository;
+import com.wecp.progressive.service.CourseService;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/course")
 public class CourseController {
-    public ResponseEntity<List<Course>> getAllCourses() {
-        return null;
+
+    private final CourseRepository courseRepository;
+
+    @Autowired
+    CourseService courseService;
+
+    CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses() throws Exception{
+        // try
+        // {
+        //     return new ResponseEntity<>(courseService.getAllCourses(),HttpStatus.OK);
+        // }
+        // catch(RuntimeException e)
+        // {
+        //     return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        // }
+        // catch(Exception ex){    
+        //      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        // }
+       if(!courseService.getAllCourses().isEmpty()){
+            return new ResponseEntity<>(courseService.getAllCourses(),HttpStatus.OK);
+       }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        
+        
+    }
+    @GetMapping("/{courseId}")
+    public ResponseEntity<Course> getCourseById(@PathVariable int courseId) throws Exception{
+    //     try{
+    //         return new ResponseEntity<>(courseService.getCourseById(courseId),HttpStatus.OK);
+    //     }
+    //     catch(RuntimeException e){
+    //         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    //     }
+    //     catch(Exception ex){    
+    //         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    //    }
+        if(courseService.getCourseById(courseId)!= null){
+            return new ResponseEntity<>(courseService.getCourseById(courseId),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
     }
 
-    public ResponseEntity<Course> getCourseById(int courseId) {
-        return null;
-    }
+    @PostMapping
+    public ResponseEntity<Integer> addCourse(Course course) throws Exception{
+    //     try{
+    //         return new ResponseEntity<>(courseService.addCourse(course),HttpStatus.CREATED);
+    //     }
 
-    public ResponseEntity<Integer> addCourse(Course course) {
-        return null;
+    //     catch(RuntimeException e){
+            
+    //     }
+    //     catch(Exception ex){    
+    //         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    //    }
+        if(!courseService.getAllCourses().isEmpty()){
+            return new ResponseEntity<>(courseService.addCourse(course),HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     public ResponseEntity<Void> updateCourse(int courseId, Course course) {
--- backend/src/main/java/com/wecp/progressive/repository/EnrollmentRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/EnrollmentRepository.java
@@ -1,5 +1,21 @@
 package com.wecp.progressive.repository;
 
+import java.util.List;
+import java.util.Optional;
 
-public interface EnrollmentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wecp.progressive.entity.Enrollment;
+
+public interface EnrollmentRepository extends JpaRepository<Enrollment,Integer>{
+    
+    Optional<Enrollment> findByStudent_StudentIdAndCourse_CourseId(Integer studentId,Integer courseId);
+    List<Enrollment> findAllByStudent_StudentId(Integer studentId);
+    List<Enrollment> findAllByCourse_CourseId(Integer courseId);
+
+    // void deleteByCourseId(int courseId);
+    // void deleteByStudentId(int studentId);
+    // void deleteByTeacherId(int teacherId);
+
+
 }
