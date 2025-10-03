020a4eea23a817864915bfe45b60fef3b6feccd6
username <wecp-dummy@gmail.com>
2025-10-03 19:33:06 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/service/impl/TeacherServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/TeacherServiceImplJpa.java
@@ -1,5 +1,67 @@
 package com.wecp.progressive.service.impl;
 
-public class TeacherServiceImplJpa  {
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.wecp.progressive.entity.Teacher;
+// import com.wecp.progressive.exception.CourseNotFoundException;
+import com.wecp.progressive.exception.TeacherAlreadyExistsException;
+import com.wecp.progressive.repository.TeacherRepository;
+import com.wecp.progressive.service.TeacherService;
+
+@Service
+public class TeacherServiceImplJpa implements TeacherService {
+
+    
+    TeacherRepository teacherRepository;
+    
+    public TeacherServiceImplJpa(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+
+    }
+
+    @Override
+    public Integer addTeacher(Teacher teacher) throws Exception {
+        if(teacherRepository.findById(teacher.getTeacherId()).isPresent()){
+            throw new TeacherAlreadyExistsException("id not found");
+        }
+        Teacher t = teacherRepository.save(teacher);
+       return t.getTeacherId();
+    }
+
+    @Override
+    public List<Teacher> getAllTeachers() throws Exception {
+        return teacherRepository.findAll();
+    }
+    public Teacher getTeacherById(int teacherId) throws Exception
+    {
+        return teacherRepository.findById(teacherId).get();
+    }
+    public void deleteTeacher(int teacherId)throws Exception
+    {
+        teacherRepository.deleteById(teacherId);
+    }
+
+    @Override
+    public List<Teacher> getTeacherSortedByExperience() throws Exception {
+        List<Teacher> ls= teacherRepository.findAll();
+        Collections.sort(ls);
+        return ls;
+    }
+    public void updateTeacher(Teacher teacher) throws Exception
+    {
+        if(teacherRepository.findById(teacher.getTeacherId()).get()!=null)
+        {
+            teacherRepository.save(teacher);
+        }
+        else
+        {
+            throw new Exception("Teacher not found");
+        }
+    }
+    
+
 
 }
--- backend/src/main/java/com/wecp/progressive/service/impl/UserLoginServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/UserLoginServiceImpl.java
@@ -1,23 +1,60 @@
 package com.wecp.progressive.service.impl;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.wecp.progressive.dto.UserRegistrationDTO;
 import com.wecp.progressive.entity.User;
-
-public class UserLoginServiceImpl {
-
-    User createUser(User user) {
-        return null;
+import com.wecp.progressive.repository.UserRepository;
+ @Service
+public class UserLoginServiceImpl implements UserDetailsService{
+ 
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+   public  User createUser(User user) {
+     return userRepository.save(user);
     }
-
-    User getUserByUsername(String username) {
-        return null;
+ 
+   public  User getUserByUsername(String username) {
+    return userRepository.findByUsername(username);
     }
+ 
+   public void registerUser(UserRegistrationDTO userRegistrationDTO) throws Exception{
+       if(userRepository.findByUsername(userRegistrationDTO.getUsername())!=null){
+        throw new Exception("Username already exists");
+       }
+       User user=new User();
+       user.setUsername(userRegistrationDTO.getUsername());
+       user.setPassword(passwordEncoder.encode(userRegistrationDTO.getPassword()));
+       user.setRole(userRegistrationDTO.getRole());
 
-    void registerUser(UserRegistrationDTO userRegistrationDTO) {
+       userRepository.save(user);
 
+ 
     }
+ 
+         public  User getUserDetails(int userId) {
+           return userRepository.findById(userId).get();
+    }
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+      User user=userRepository.findByUsername(username);
+      if(user==null){
+        throw new UsernameNotFoundException("User not found");
+      }
+      return org.springframework.security.core.userdetails.User
+      .withUsername(user.getUsername())
+      .password(user.getPassword())
+      .roles(user.getRole())
+      .build();
 
-    User getUserDetails(int userId) {
-        return null;
     }
 }
+ 
+ 
