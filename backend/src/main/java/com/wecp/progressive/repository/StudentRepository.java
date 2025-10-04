03619022696cf53bf53f6c65dbec6a10ffc06f7f
username <wecp-dummy@gmail.com>
2025-10-04 05:33:05 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/repository/StudentRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/StudentRepository.java
@@ -1,5 +1,12 @@
 package com.wecp.progressive.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface StudentRepository {
+import com.wecp.progressive.entity.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+    public Student findByStudentId(int studentId);
+
 }
