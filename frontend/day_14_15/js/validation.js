888c448c9bd57cd570f143b784c919b4345762fc
username <wecp-dummy@gmail.com>
2025-10-07 17:13:35 +0000

    wecp initial commit

--- frontend/day_14_15/js/validation.js
+++ frontend/day_14_15/js/validation.js
@@ -1,18 +1,25 @@
 function login() {
-   
+   let username = document.getElementById("loginUsername").value;
+   let password = document.getElementById("loginPassword").value;
     // You can perform login validation and authentication here
     // For simplicity, let's just display an alert
+    console.log(`Login clicked. Username: ${username}, Password: ${password}`);
     
 }
 
 function register() {
    
-
+    let registerName = document.getElementById("registerName").value;
+    let registerEmail = document.getElementById("registerEmail").value;
+    let registerUsername = document.getElementById("registerUsername").value;
+    let registerPassword = document.getElementById("registerPassword").value;
     // Frontend validation for registration form
-    
+    console.log(`Register clicked. Name: ${registerName}, Email: ${registerEmail}, Username: ${registerUsername}, Password: ${registerPassword}`);
 
     // Validate email format
-    
+    // if(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]/.test(registerEmail)){
+    //     alert("Email Format wrong");
+    // }
     // Validate username (no special characters)
     
 
