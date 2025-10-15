1d720e89c11589a9c0511acf028b9413a3ceaeeb
username <wecp-dummy@gmail.com>
2025-10-15 04:28:21 +0000

    wecp initial commit

--- frontend/src/app/educonnect/components/studentcreate/studentcreate.component.ts
+++ frontend/src/app/educonnect/components/studentcreate/studentcreate.component.ts
@@ -1,4 +1,39 @@
+import { Component } from "@angular/core";
+import { Student } from "../../models/Student";
 
+@Component({
+    selector:'student-create',
+    templateUrl:'./studentcreate.component.html',
+    styleUrls:['./studentcreate.component.scss']
+})
 export class StudentCreateComponent {
- 
+    student: Student;
+    successMessage: string | null;
+    errorMessage: string | null;
+
+    constructor()
+    {
+        this.student=new Student(0,"",null,"","","")
+    }
+
+    onSubmit(): void
+    {
+        if(!this.student.fullName || !this.student.dateOfBirth || !this.student.email || !this.student.contactNumber || !this.student.address)
+        {
+            this.successMessage=null;
+            this.errorMessage="Please fill in all required fields."
+        }
+        else
+        {
+            this.successMessage="Student created successfully!";
+            this.errorMessage=null;
+        }
+    }
+
+    resetForm(): void
+    {
+        this.student=new Student(0,"",null,"","","")
+        this.successMessage=null;
+        this.errorMessage=null;
+    }
 }
