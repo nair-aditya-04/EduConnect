318e4099a55142a5cd4316f837c73c1b217f370a
Aditya Nair <nair.aditya.lti@gmail.com>
2025-10-19 12:13:09 +0530

    Backend Testing

--- backend/src/main/java/com/wecp/progressive/exception/CourseAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.wecp.progressive.exception;
-
-public class CourseAlreadyExistsException extends RuntimeException{
-    public CourseAlreadyExistsException(String message){
-        super(message);
-    }
-}
